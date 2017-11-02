e0d9a7f5d4659babfd4408d470b0a472f228438f
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-03 01:52:26 +0300

    gg

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 ﻿<?php
 
 $output = json_decode(file_get_contents('php://input'),true);
-file_put_contents("logs.txt", $output);
+$id = $output['message']['chat']['id'];
+
+file_put_contents("logs.txt", $id);
 
 ?>
