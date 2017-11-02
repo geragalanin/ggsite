eac4bc90247c7a0ec9266eda85c274f7e0a6754b
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-03 02:32:30 +0300

    gg

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
-$text = $output['message']['text'];
+$message = $output['message']['text'];
 $token = '384628942:AAFoapuIipUZEAwi2NQoNElgF6uXfBdWFu8';
 
-function sendMessage($token, $id){
-	file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=ih");
+function sendMessage($id, $message){
+	file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$message);
 }
 
 sendMessage($token, $id);
