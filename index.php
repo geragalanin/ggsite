58b4f93efacfd1cd89a64645599f3f213a8183f2
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-03 16:47:54 +0300

    gg

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ switch($text){
 	case 'hi':
 		$message = 'hello';
 		sendMessage($id, $message);
+		file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$message);
 }
 
 function sendMessage($id, $message){
