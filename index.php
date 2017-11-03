32ae30c4620a47e7551b254ae92baacfee941fe7
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-03 03:20:36 +0300

    gg

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
-$text = $output['message']['text'];
+$message = $output['message']['text'];
 $token = '384628942:AAFoapuIipUZEAwi2NQoNElgF6uXfBdWFu8';
 
 function sendMessage($token, $id, $message){
 	file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$message);
 }
 
-		sendMessage($id, $text);
+switch($message){
+	case 'hi':
+		$message = 'hello';
+		sendMessage($id, $message);
+	break;
+	case 'hihi':
+		$message = 'hellohello';
+		sendMessage($id, $message);
+	break;
+	
+	default:
+		$message = 'no';
+		sendMessage($id, $message);
+}
 
 ?>
