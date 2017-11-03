fb733ee17c1c72ec6fd9b767d2ef0143a7dabc89
geragalanin <32804078+geragalanin@users.noreply.github.com>
2017-11-03 16:46:52 +0300

    gg

--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
+$text = $output['message']['text'];
 $token = '384628942:AAFoapuIipUZEAwi2NQoNElgF6uXfBdWFu8';
 
-switch($message){
+switch($text){
 	case 'hi':
 		$message = 'hello';
-		sendMessage($id, $message);
-	break;
-	case 'hihi':
-		$message = 'hellohello';
-		sendMessage($id, $message);
-	break;
-	
-	default:
-		$message = 'no';
 		sendMessage($id, $message);
 }
 
