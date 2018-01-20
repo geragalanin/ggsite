d9b98485d8194a512bf549b755e9127ecaae67b0
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-20 17:26:04 +0300

    3

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-file_get_contents("http://ggsite.ru/bot/ggsite/index.php?pass=0982359238&id=".$id."&text=".urlencode($text));
+file_get_contents("http://ggsite.ru/bot/ggsite/index.php?pass=76898428&id=".$id."&text=".urlencode($text));
 
 ?>
