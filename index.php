526ee30d8e87cffc4642e9f02712b4acfc31643a
geragalanin <32804078+geragalanin@users.noreply.github.com>
2018-01-20 17:01:08 +0300

    2

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
 file_get_contents("http://ggsite.ru/bot/ggsite/index.php?pass=0982359238&id=".$id."&text=".urlencode($text));
+file_get_contents("https://api.telegram.org/bot384628942:AAFoapuIipUZEAwi2NQoNElgF6uXfBdWFu8/sendMessage?chat_id=".$id."&text=".$id);
 
 ?>
